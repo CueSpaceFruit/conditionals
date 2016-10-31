bfa1acbcffbbeb6c0e9a17e158c0fea73a631d54
Cue Lai <cuelai@gmail.com>
2016-10-31 21:52:47 +1000

    add conditionals 2 tutorial

--- Conditionals.playground/Contents.swift
+++ Conditionals.playground/Contents.swift
@@ -4,4 +4,67 @@ import UIKit
 
 var str = "Hello, playground"
 
-var isMyHouseOnFire: Bool = false
+var isMyHouseOnFire: Bool = false //explicit
+
+var anotherBool = true //not explicit
+
+if  isMyHouseOnFire {
+    
+    print ("Someone get me some water")
+    
+} else {
+    
+    print("Someone get some fire for my house")
+}
+
+
+var result = true == true  //== is used for checking single = is to assign - in this case true is equal to true therefore result returns true
+
+result = true == false // in this case true does not equal false therefore result returns false
+
+result = false == false
+
+result = 1 == 1
+
+var acctTotal = 300.33
+var newCallOfDutyGAme = 59.99
+let purchaseUnsuccessful: String = "Your purchase was unsuccessful, please check your account balance and try again"
+
+if acctTotal >= newCallOfDutyGAme {
+    print("Purchase Successful")
+} else {
+    print(purchaseUnsuccessful)
+}
+
+var name = "Jackie"
+
+if name == "Jack" {
+    print("Your Name Is Jack!!")
+} else {
+    print("Your name is not Jack")
+}
+
+var numberA = 25
+var numberB = 100
+var numberC = 45
+var numberD = 50
+
+var finalNumber = 100
+
+if numberA == finalNumber {
+
+    print(numberA)
+
+} else if numberB == finalNumber {
+    
+    print(numberB)
+
+} else if numberC == finalNumber {
+    
+    print(numberC)
+
+} else {
+
+    print("none of the values were equal to the final number")
+
+}
---
+++ conditionals2.playground/Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myAcct = 1000.00
+var myFriendsAcct = 2000.00
+var myFriendsFriendsAcct = 3000.00
