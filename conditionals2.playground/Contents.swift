2938c72ac040b7458806e5e13bf7d490e83c6191
Cue Lai <cuelai@gmail.com>
2016-10-31 22:16:33 +1000

    updated code for conditionals2

--- conditionals2.playground/Contents.swift
+++ conditionals2.playground/Contents.swift
@@ -5,3 +5,15 @@ import UIKit
 var myAcct = 1000.00
 var myFriendsAcct = 2000.00
 var myFriendsFriendsAcct = 3000.00
+
+if myAcct > 900 && myFriendsAcct > 1500 &&  myFriendsFriendsAcct > 2000 {
+    print("we've got moocho moola")
+}
+
+var playerAAlive = true
+var playerBAlive = false
+var playerCAlive = true
+
+if playerAAlive == false || playerBAlive == false || playerCAlive == false {
+    print("one of your team members is down!")
+}
